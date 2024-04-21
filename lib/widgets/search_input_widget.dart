4a86a2bce1487efc3a81368c7577fe8ca9af187f
Rociozvl <rociogalbanshinox.com>
2024-04-21 19:13:22 -0300

    changes3

--- lib/widgets/search_input_widget.dart
+++ lib/widgets/search_input_widget.dart
@@ -1,27 +1,32 @@
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-
-
-
-
-
+import 'package:flutter_typeahead/flutter_typeahead.dart';
 
 
 class SearchInputWidget extends StatelessWidget {
-  const SearchInputWidget({key});
+  const SearchInputWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+  
+    String buscarLugar = ''; 
+    
+   return Container( 
+
                   width: 150,
                   height: 50,
+                  //TODO:  CAMBAIR A MENU DROPDOWN ¿? 
                   child: TextFormField(
                     decoration:const InputDecoration(
                       label: Text('¿a dónde voy?'),
                     ),
+                    onChanged: (value) => buscarLugar = value ,
+                    onEditingComplete: () async {
+                     
+                    },
                   ),
                 );
-    
+   }
   }
-}
